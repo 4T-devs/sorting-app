7b73784ae38f27bf8881855ed4062cc3ffcccd19
Shelden-rc <shelsheldeb@gmail.com>
2025-09-15 14:44:15 +0300

    Доработка интерфейса под модель

--- src/main/java/com/ftdevs/sortingapp/BinarySearch/BinarySearchApplication.java
+++ src/main/java/com/ftdevs/sortingapp/BinarySearch/BinarySearchApplication.java
@@ -1,12 +1,16 @@
 package com.ftdevs.sortingapp.BinarySearch;
 
+import com.ftdevs.sortingapp.model.Product;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class BinarySearchApplication { //todo change generic into product, uncomment the search calls
-    private BinarySearchApplication () {}
-    public static <T>void binarySearch(T[] products, Scanner scanner){
+public class BinarySearchApplication {
+
+    private BinarySearchApplication() {}
+    //todo if it is necessary to change the declaration of the comparator,change sort method
+    public static void binarySearch(Product[] products, Scanner scanner) {
         while (true) {
             System.out.println("\nВыберите поле для поиска:");
             System.out.println("1 - Артикул");
@@ -17,36 +21,48 @@ public class BinarySearchApplication { //todo change generic into product, uncom
 
             String choice = scanner.nextLine().trim();
             if (choice.equals("0")) {
-                System.out.println("Выход из программы.");
+                System.out.println("Выход из поиска.");
                 break;
             }
 
-            switch (choice) { //todo if it is necessary to change the declaration of the comparator,change sort method
+            switch (choice) {
                 case "1" -> {
-                    //Arrays.sort(products, Comparator.comparing(T::getArticle));
+                    Arrays.sort(products, Comparator.comparing(Product::getSku));
                     System.out.print("Введите артикул: ");
                     String article = scanner.nextLine().trim();
-                    //T key = new T.Builder().article(article).name("X").price(0).build();
-                    //int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparing(T::getArticle));
-                    //printResult(products, idx);
+                    Product key = Product.builder()
+                            .sku(article)
+                            .name("dummy")
+                            .price(0.0)
+                            .build();
+                    int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparing(Product::getSku));
+                    printResult(products, idx);
                 }
                 case "2" -> {
-                    //Arrays.sort(products, Comparator.comparing(T::getName));
+                    Arrays.sort(products, Comparator.comparing(Product::getName));
                     System.out.print("Введите название: ");
                     String name = scanner.nextLine().trim();
-                    //T key = new T.Builder().article("X").name(name).price(0).build();
-                    //int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparing(T::getName));
-                    //printResult(products, idx);
+                    Product key = Product.builder()
+                            .sku("dummy")
+                            .name(name)
+                            .price(0.0)
+                            .build();
+                    int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparing(Product::getName));
+                    printResult(products, idx);
                 }
                 case "3" -> {
-                    //Arrays.sort(products, Comparator.comparingDouble(T::getPrice));
+                    Arrays.sort(products, Comparator.comparingDouble(Product::getPrice));
                     System.out.print("Введите цену: ");
                     String priceStr = scanner.nextLine().trim();
                     try {
                         double price = Double.parseDouble(priceStr);
-                        //T key = new Product.Builder().article("X").name("X").price(price).build();
-                        //int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparingDouble(T::getPrice));
-                        //printResult(products, idx);
+                        Product key = Product.builder()
+                                .sku("dummy")
+                                .name("dummy")
+                                .price(price)
+                                .build();
+                        int idx = BinarySearchUtil.binarySearch(products, key, Comparator.comparingDouble(Product::getPrice));
+                        printResult(products, idx);
                     } catch (NumberFormatException e) {
                         System.out.println("Ошибка: введите корректное число.");
                     }
@@ -54,15 +70,13 @@ public class BinarySearchApplication { //todo change generic into product, uncom
                 default -> System.out.println("Некорректный выбор. Попробуйте снова.");
             }
         }
-
     }
 
     private static <T> void printResult(T[] products, int idx) {
         if (idx >= 0) {
-            System.out.println("Найден объект: " + products[idx].toString());
+            System.out.println("Найден объект: " + products[idx]);
         } else {
             System.out.println("Элемент не найден.");
         }
     }
 }
-
