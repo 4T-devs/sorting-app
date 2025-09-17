8ce6e7cc98039d80d4c68f081a4cb92084b8807a
Th3Spac3 <thespaceal@gmail.com>
2025-09-17 17:29:48 +0300

    fix checkstyle

--- src/main/java/com/ftdevs/sortingapp/IOSingleton.java
+++ src/main/java/com/ftdevs/sortingapp/IOSingleton.java
@@ -4,8 +4,7 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public final
-class IOSingleton { // Синглтон для ввода и вывода, создан для того, чтобы обеспечить удобство при
+public final class IOSingleton { // Синглтон для ввода и вывода, создан для того, чтобы обеспечить удобство при
     // тестировании
 
     private static IOSingleton instance;
