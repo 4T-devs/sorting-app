9348d73dd190f8ced9584ccc44d1d9f6595af92f
Shelden-rc <shelsheldeb@gmail.com>
2025-09-21 19:48:10 +0300

    Added a sort call before searching

--- src/main/java/com/ftdevs/sortingapp/searchui/SearchService.java
+++ src/main/java/com/ftdevs/sortingapp/searchui/SearchService.java
@@ -4,14 +4,17 @@ import com.ftdevs.sortingapp.collections.CustomList;
 import com.ftdevs.sortingapp.comparator.ProductComparators;
 import com.ftdevs.sortingapp.model.Product;
 import com.ftdevs.sortingapp.search.BinarySearchUtil;
+import com.ftdevs.sortingapp.sorting.strategy.QuickSort;
 import java.util.Optional;
 
 public final class SearchService {
+    private static QuickSort<Product> quickSort = new QuickSort<>();
 
     private SearchService() {}
 
     public static Optional<Integer> findBySku(
             final CustomList<Product> products, final String sku) {
+        quickSort.sort(products, ProductComparators.BY_SKU);
         final Product key = Product.builder().sku(sku).name("dummy").price(0.0).build();
         final int idx = BinarySearchUtil.binarySearch(products, key, ProductComparators.BY_SKU);
         return idx >= 0 ? Optional.of(idx) : Optional.empty();
@@ -19,6 +22,7 @@ public final class SearchService {
 
     public static Optional<Integer> findByName(
             final CustomList<Product> products, final String name) {
+        quickSort.sort(products, ProductComparators.BY_NAME);
         final Product key = Product.builder().sku("dummy").name(name).price(0.0).build();
         final int idx = BinarySearchUtil.binarySearch(products, key, ProductComparators.BY_NAME);
         return idx >= 0 ? Optional.of(idx) : Optional.empty();
@@ -26,6 +30,7 @@ public final class SearchService {
 
     public static Optional<Integer> findByPrice(
             final CustomList<Product> products, final double price) {
+        quickSort.sort(products, ProductComparators.BY_PRICE);
         final Product key = Product.builder().sku("dummy").name("dummy").price(price).build();
         final int idx = BinarySearchUtil.binarySearch(products, key, ProductComparators.BY_PRICE);
         return idx >= 0 ? Optional.of(idx) : Optional.empty();
